7a2c744a7abe00edb4272f0333075cf35e595d8b
levibyte <kartol@gmx.com>
2019-01-19 12:52:31 +0100

    add posibility of runtime callbacktype support ( still possible to typedef to enums )

--- src/skeleton/postman.h
+++ src/skeleton/postman.h
@@ -1,51 +1,13 @@
 #ifndef postman_h
 #define postman_h
 
+#include "callback.h"
+
 #include <map>
 #include <vector>
-//#include <string>
+#include <string>
 #include <functional>
 
-class LeCallbackData 
-{
-    public:
-        virtual ~LeCallbackData() {}
-};
-
-class customdata: public LeCallbackData 
-{
-    public:
-        customdata(int i):m_i(i) {}
-        int get() { return m_i; }
-
-    private:
-        int m_i;
-};
-
-typedef std::function<void(LeCallbackData&)> callBackFun1;
-typedef std::function<void()> callBackFun2;
-
-enum LeCallbackType {CB_GAMEOVER};
-
-class LeCallback 
-{
-    friend class LePostman;
-    
-    public:
-        void purge();
-        void call();
-        void call(LeCallbackData& data);
-        
-    private:
-        LeCallback(callBackFun1 f1, int id, LeCallbackType t);
-        LeCallback(callBackFun2 f2, int id, LeCallbackType t);
-        
-    private:
-        callBackFun1 m_f1;
-        callBackFun2 m_f2;
-        int m_internal_id;
-        LeCallbackType m_type;
-};
 
 class LePostman
 {
@@ -56,20 +18,20 @@ class LePostman
         };
     
     public:
-        //void register_callback(LeCallbackType,std::string,callBackFun1 f);
-        //void register_callback(LeCallbackType,std::string,callBackFun2 f);
-        //void unregister_callback(????);
+        LeCallbackType add_callback_type(const std::string&);
+        //LeCallbackType get_callback_type(const std::string&);
         
-        //template<typename T>
-        LeCallback register_callback(LeCallbackType t, callBackFun1 f);
-        LeCallback register_callback(LeCallbackType t, callBackFun2 f);
-        void deregister_callback(LeCallbackType t, int id);
+        LeCallback register_callback(const LeCallbackType& t, callBackFun1 f);
+        LeCallback register_callback(const LeCallbackType& t, callBackFun2 f);
+        void deregister_callback(LeCallbackType& t, int id);
 
-        void notify(LeCallbackType);
-        void notify(LeCallbackType, LeCallbackData&);
+        void notify(LeCallbackType&);
+        void notify(LeCallbackType&, LeCallbackData&);
 
     private:
         std::map<LeCallbackType,std::vector<LeCallback>> m_type2vecfun;
+        std::map<int,LeCallbackType> m_index2type;
+        int m_types_count;
         
     public:
         static LePostman* m_instance;
